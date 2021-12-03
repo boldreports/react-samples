f68aa62642ef8bca76cc8ff038a3874e7b2b2aa9
Subhasree Vasudevan <subhasree.vasudevan@syncfusion.com>
2021-12-03 19:05:23 +0530

    BoldReports v3.3.23 changes

--- src/globals.js
+++ src/globals.js
@@ -59,8 +59,8 @@ function EDIT_REPORT(args) {
     }
 }
 
-var ServiceURL = '/demos/services/api/ReportViewerWebApi';
-var DesignerServiceURL = '/demos/services/api/ReportDesignerWebApi';
+var ServiceURL = 'https://demos.boldreports.com/services/api/ReportViewerWebApi';
+var DesignerServiceURL = 'https://demos.boldreports.com/services/api/ReportDesignerWebApi';
 
 const SampleComponents = {
     ProductLineSales: ProductLineSales,
