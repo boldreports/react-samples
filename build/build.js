1182fd6d0a754e10dc6bc6cf3cbfb3c3f0457492
Sundaresan Gunasekaran <sundaresan.gunasekaran@syncfusion.com>
2023-07-19 13:46:53 +0530

    feature(REPORT-320537): Bold-Reports 5.2 Release Changes

--- build/build.js
+++ build/build.js
@@ -3,7 +3,7 @@ const runSequence = require('gulp4-run-sequence');
 var shelljs = require('shelljs');
 
 gulp.task('pre-build', function (done) {
-    runSequence('update-barcode', 'clean', 'copy-src-assets', done);
+    runSequence('update-barcode', 'clean','copy-dependent-scripts', 'copy-src-assets', done);
 })
 
 gulp.task('build', function (done) {
